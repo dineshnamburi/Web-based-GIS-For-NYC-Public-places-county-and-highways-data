38be78bef75dfa75ee415568e0d90252c419ef76
dineshnamburi <n.dinesh36@gmail.com>
2016-10-16 10:23:02 -0400

    Added map files and map templates to generate maps to show symmetrical differences between newyork city borough structures

---
+++ mapscripts/question6_1.php
@@ -0,0 +1,28 @@
+<?php
+
+if (!extension_loaded("MapScript")) dl("php_mapscript.so");
+
+$request = ms_newowsrequestobj();
+
+foreach ($_GET as $k=>$v) {
+     $request->setParameter($k, $v);
+}
+
+$request->setParameter("VeRsIoN","1.0.0");
+ms_ioinstallstdouttobuffer();
+$oMap = ms_newMapobj("C:/ms4w/Apache/htdocs/proj1/maps/question6_1.map");
+
+$oMap->owsdispatch($request);
+$contenttype = ms_iostripstdoutbuffercontenttype();
+if ($contenttype == 'image/png')
+{
+   header('Content-type: image/png');
+   ms_iogetStdoutBufferBytes();
+}
+else
+{
+	$buffer = ms_iogetstdoutbufferstring();
+	echo $buffer;
+}
+ms_ioresethandlers();
+?>
