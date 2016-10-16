dee66be7f1dc8965386e942dce2205378809b86c
dineshnamburi <n.dinesh36@gmail.com>
2016-10-16 09:27:58 -0400

    Implemeted a map to show highways in newyork city

---
+++ mapscripts/template.php
@@ -0,0 +1,26 @@
+<?php
+
+$request = ms_newowsrequestobj();
+
+foreach ($_GET as $k=>$v) {
+     $request->setParameter($k, $v);
+}
+
+$request->setParameter("VeRsIoN","1.0.0");
+ms_ioinstallstdouttobuffer();
+$oMap = ms_newMapobj("/home/zhang/public_html/proj1/maps/template.map");
+
+$oMap->owsdispatch($request);
+$contenttype = ms_iostripstdoutbuffercontenttype();
+if ($contenttype == 'image/png')
+{
+   header('Content-type: image/png');
+   ms_iogetStdoutBufferBytes();
+}
+else
+{
+	$buffer = ms_iogetstdoutbufferstring();
+	echo $buffer;
+}
+ms_ioresethandlers();
+?>
